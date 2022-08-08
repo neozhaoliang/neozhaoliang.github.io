7ad535ea1910279a45bdace14a2368a9a8db75c6
neozhaoliang <mathzhaoliang@gmail.com>
2022-08-08 15:17:08 +0800

    Site updated: 2022-08-08 15:17:07

--- book/make.sh
+++
@@ -1,6 +0,0 @@
-rst2html.py --link-stylesheet            \
-            --cloak-email-addresses      \
-            --toc-top-backlinks          \
-            --stylesheet book.css        \
-            --stylesheet-dirs .          \
-            book.rst index.html
