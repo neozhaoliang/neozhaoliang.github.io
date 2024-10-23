7dee50f98112334de5c0bae32bf0423c15ec5267
neozhaoliang <mathzhaoliang@gmail.com>
2024-10-23 14:35:02 +0800

    Site updated: 2024-10-23 14:34:57

--- images/rouche/open_mapping.py
+++ images/rouche/open_mapping.py
@@ -1,59 +1,41 @@
-from piscript.PiModule import *
+import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np
 
-init("test.eps", 200, 200)
-center()
-scale(45)
-translate(0.25, -0.3)
+plt.rcParams.update({"text.usetex": True, "font.family": "Helvetica"})
+
+plt.gcf().set_size_inches(3, 3)
+plt.axis("off")
+plt.gca().set_aspect("equal")
+
 
 def f(z):
     a = -0.5 + 0.5j
-    return (z - a) / (1 - z * a.conjugate()) * (z - 1  - 1j)
+    return (z - a) / (1 - z * a.conjugate()) * (z - 1 - 1j)
+
 
 N = 1000
 pts = np.linspace(0, 2 * np.pi, N)
 pts = np.exp(1j * pts)
 pts = [f(z) for z in pts]
-newpath()
-moveto(pts[0].real, pts[0].imag)
-for i in range(1, N):
-    lineto(pts[i].real, pts[i].imag)
-closepath()
-stroke()
+xx = [p.real for p in pts]
+yy = [p.imag for p in pts]
+
+plt.plot(xx, yy, "k-", lw=0.8)
 
 rad = 0.05
-
 w0 = 0.1 + 1j
 w1 = 0.8 + 1j
-newpath()
-circle(w0.real, w0.imag, rad)
-fill(0)
-
-newpath()
-circle(w1.real, w1.imag, rad)
-fill(0)
-
-setdash([4, 4])
-newpath()
-circle(w0.real, w0.imag, abs(w0-w1))
-stroke()
-
-
-
-sc = 1.2
-
-t1 = texinsert(r"$w_0$")
-t1.scale(sc)
-place(t1, w0.real - 0.3, w0.imag - 0.25)
-
-
-t2 = texinsert(r"$w_1$")
-t2.scale(sc)
-place(t2, w1.real+0.1, w1.imag - 0.25)
-
-
-t3 = texinsert(r"$f(\gamma)$")
-t3.scale(sc)
-place(t3, -2, 1)
-
-flush()
+plt.plot(w0.real, w0.imag, "ko", markersize=3)
+plt.plot(w1.real, w1.imag, "ko", markersize=3)
+c = plt.Circle(
+    (w0.real, w0.imag), radius=abs(w0 - w1), ls="--", fc="none", ec="k", lw=0.8
+)
+c.set_linestyle((0, (4, 4)))
+plt.gca().add_patch(c)
+fs = 16
+plt.text(w0.real - 0.3, w0.imag - 0.3, r"$w_0$", fontdict={"fontsize": fs})
+plt.text(w1.real + 0.05, w0.imag - 0.3, r"$w_1$", fontdict={"fontsize": fs})
+plt.text(-2, 1, r"$f(\gamma)$", fontdict={"fontsize": fs})
+plt.gcf().tight_layout()
+plt.savefig("winding_number.svg", transparent=True, bbox_inches="tight")
